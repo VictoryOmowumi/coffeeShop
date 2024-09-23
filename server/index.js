80d74c49d733c8ee750bf5cbf71d65b8e851999b
victory balogun <victorybalogun@gmail.com>
2024-09-23 11:10:46 +0100

    test6

--- server/index.js
+++ server/index.js
@@ -19,19 +19,13 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/customers', customerRoutes)
 
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
