06cf44618f23dd194acd8d1ac5839333253f80e6
victory balogun <victorybalogun@gmail.com>
2024-09-23 11:28:59 +0100

    test7

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
