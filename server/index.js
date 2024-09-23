1d7aff5ebf7815b3e0cd870c963d1dae349e78b0
victory balogun <victorybalogun@gmail.com>
2024-09-23 11:07:03 +0100

    test5

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
